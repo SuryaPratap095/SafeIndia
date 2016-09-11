f59ae62bb97e64e80c0f5b41e426d8f7f02449f3
keithgeorge <keithgeorge095@gmail.com>
2016-09-11 13:35:00 +0530

    Add config

---
+++ app/src/main/java/com/example/surya/safeindia/config.java
@@ -0,0 +1,10 @@
+package com.example.surya.safeindia;
+
+/**
+ * Created by surya on 11/9/16.
+ */
+public class config {
+    public static String URL_USER="http://safetrk.esy.es/save_user.php";
+
+
+}
